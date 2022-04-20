d400f0c08e90ff56041d59fef6f1ae08e3f55379
mng cluster <dimensionwebstudio@gmail.com>
2022-04-20 17:56:38 +0000

    update

---
+++ js/code.js
@@ -0,0 +1,11 @@
+/////////////////////NAVBAR START//////////////////////////////////////////////////
+
+function LightHome() {
+    window.location.replace("/Pages/light.html");
+}
+
+function DarkHome() {
+    window.location.replace("https://5500-dimensionwe-mymemorycol-a6wpi5if8bz.ws-eu34.gitpod.io/index.html");
+}
+
+/////////////////////NAVBAR END////////////////////////////////////////////////////
